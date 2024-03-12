32f96872052dce5b0413b444a0a9616ec4905e89
Aashish kumar <138276486+Aashishkr006@users.noreply.github.com>
2024-03-12 14:31:27 +0000

    container confi done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "aashish@gmail.com"
+var accountPassword = "123456"
+accountCity = "Bhopal"
+let accountState;
+
+// accountId = 2; //Not allowed
+
+accountEmail= "adsv@g";
+accountPassword="56784"
+accountCity="patna"
+
+// console.log (accountEmail);
+console.log ([accountId, accountPassword, accountCity, accountEmail, accountState])
+/*
+Prefer not to use 'var'
+for constent use 'const' and for variable use 'let'.
+*/
