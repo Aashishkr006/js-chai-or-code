c7a27ad89b7590b4e8120695fa46473fdb78cbe6
Aashish kumar <138276486+Aashishkr006@users.noreply.github.com>
2024-03-12 19:24:08 +0000

    today's task is done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// alert (3+5)
+//this wil not work because we are using nodejs not browser console, no popup wull be happen as happen in console.
+
+//number => 2^approx 53 or something
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone vlaue
+//undefine => value not assigned
+//symbol => for unique
+
+console.log (typeof "Aashish"); //string
+console.log (typeof null); //object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "34";
+
+console.log(typeof score);
+console.log(typeof(score)); //both are same
+
+// ****** conversions*********//
+
+let englishScore = Number(score);
+
+console.log(typeof englishscore);
+console.log(englishScore);
+
+// ** if **//
+
+let scores = "34abc";
+
+let englishScores = Number(scores);
+
+console.log(typeof englishscores);
+console.log(englishScores);
+// NaN => not a numbre //
+
+let answer = true;
+
+console.log(typeof answer);
+let realanswer = Number(answer);
+console.log(realanswer);
+
+//**********Operationa***********//
+
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(2**5);//2^5
+// console.log(2%5);//for reminder check
+
+console.log(2+5);
+console.log("2"+5);
+console.log("2"+"5");
+console.log("2"+5+5);
+console.log(5+5+"2");
+
+//read about 'prefix and postfix increment'//
+
+
+
+
+
