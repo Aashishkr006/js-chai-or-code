bad12ee5709bd127cbea77b8bd13f7ca47c5b651
Aashish kumar <138276486+Aashishkr006@users.noreply.github.com>
2024-03-14 08:43:17 +0000

    done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,6 @@
+const name = "Aashish"
+const repocount = 50
+
+console.log (name + repocount +"value"); // this is not good way to write string
+
+console.log (`Hello my name is ${name} and my repo count is ${repocount}`); // this is good practice (this is string interpolation )
