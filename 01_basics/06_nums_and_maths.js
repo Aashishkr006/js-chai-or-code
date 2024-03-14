c28c7cdec3b36b540b5a1fd28c870fd7f0572ef4
Aashish kumar <138276486+Aashishkr006@users.noreply.github.com>
2024-03-14 15:25:20 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400;
+// console.log(score);
+
+// const balance = new Number(100.58);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.98745
+
+// console.log(otherNumber.toPrecision(4));
+
+const countNum = 100000000;
+// console.log(countNum.toLocaleString('en-IN'));
+
+
+// ++++++++++++ Maths +++++++++++//
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.58));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(7,5,8,2,9));
+console.log(Math.max(58,25,4,289,555));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10+1));
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min + 1)) +min);
