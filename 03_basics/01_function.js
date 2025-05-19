9d2708b37c06a71796dee3c395dd67f51af95f1b
aashishkr006 <aashishkr006@gmail.com>
2025-05-20 03:16:05 +0530

    learnt basics of function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,30 @@
+// function loginUserMessage(name){
+//     if(!name){
+//         console.log("please give me the name");
+//         return;
+//     }
+//     else{
+//         console.log(`${name} just logges in`);
+//         return
+//     }
+// }
+
+// loginUserMessage(" ");
+
+
+
+
+// function loginUserMessage(name){
+//     if(name === undefined){
+//         console.log("please give me the name");
+//         return;
+//     }
+//     else{
+//         console.log(`${name} just logges in`);
+//         return
+//     }
+// }
+
+// loginUserMessage("");
+
+// console.log(typeof "")
